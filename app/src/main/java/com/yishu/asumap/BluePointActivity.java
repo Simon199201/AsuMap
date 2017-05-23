f53ba1c7ad79d68cea1a7dc8b6c77d36b41ab41c
zhangxinmeng <zhangxinmeng@kuner.com.cn>
2017-05-23 16:35:26 +0800

    first commit

---
+++ app/src/main/java/com/yishu/asumap/BluePointActivity.java
@@ -0,0 +1,116 @@
+package com.yishu.asumap;
+
+import android.app.Activity;
+import android.graphics.Color;
+import android.os.Bundle;
+import android.widget.Toast;
+
+import com.amap.api.maps.AMap;
+import com.amap.api.maps.CameraUpdateFactory;
+import com.amap.api.maps.MapView;
+import com.amap.api.maps.model.BitmapDescriptorFactory;
+import com.amap.api.maps.model.MyLocationStyle;
+
+
+
+public class BluePointActivity extends Activity{
+	private MapView mapView;
+	private AMap aMap;
+    private static final int STROKE_COLOR = Color.argb(180, 3, 145, 255);
+    private static final int FILL_COLOR = Color.argb(10, 0, 0, 180);
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.bluepoint_activity);
+		if (!Util.isNetworkAvailable(this)) {
+			Toast.makeText(this, R.string.net_enable,Toast.LENGTH_SHORT).show();
+		}
+		/*
+         * 设置离线地图存储目录，在下载离线地图或初始化地图设置;
+         * 使用过程中可自行设置, 若自行设置了离线地图存储的路径，
+         * 则需要在离线地图下载和使用地图页面都进行路径设置
+         * */
+		//Demo中为了其他界面可以使用下载的离线地图，使用默认位置存储，屏蔽了自定义设置
+		//  MapsInitializer.sdcardDir =OffLineMapUtils.getSdCacheDir(this);
+
+		mapView = (MapView) findViewById(R.id.map);
+		mapView.onCreate(savedInstanceState);// 此方法必须重写
+
+		init();
+	}
+
+	/**
+	 * 初始化AMap对象
+	 */
+	private void init() {
+		if (aMap == null) {
+			aMap = mapView.getMap();
+			aMap.moveCamera(CameraUpdateFactory.zoomTo(19));
+            setUpMap();
+		}
+	}
+
+	/**
+	 * 方法必须重写
+	 */
+	@Override
+	protected void onResume() {
+		super.onResume();
+		mapView.onResume();
+	}
+
+	/**
+	 * 方法必须重写
+	 */
+	@Override
+	protected void onPause() {
+		super.onPause();
+		mapView.onPause();
+	}
+
+	/**
+	 * 方法必须重写
+	 */
+	@Override
+	protected void onSaveInstanceState(Bundle outState) {
+		super.onSaveInstanceState(outState);
+		mapView.onSaveInstanceState(outState);
+	}
+
+	/**
+	 * 方法必须重写
+	 */
+	@Override
+	protected void onDestroy() {
+		super.onDestroy();
+		mapView.onDestroy();
+	}
+
+    /**
+     * 设置一些amap的属性
+     */
+    private void setUpMap() {
+        aMap.getUiSettings().setMyLocationButtonEnabled(false);// 设置默认定位按钮是否显示
+        aMap.setMyLocationEnabled(true);// 设置为true表示显示定位层并可触发定位，false表示隐藏定位层并不可触发定位，默认是false
+        setupLocationStyle();
+    }
+
+    /**
+     * 设置自定义定位蓝点
+     */
+    private void setupLocationStyle(){
+        // 自定义系统定位蓝点
+        MyLocationStyle myLocationStyle = new MyLocationStyle();
+        // 自定义定位蓝点图标
+        myLocationStyle.myLocationIcon(BitmapDescriptorFactory.
+                fromResource(R.mipmap.gps_point));
+        // 自定义精度范围的圆形边框颜色
+        myLocationStyle.strokeColor(STROKE_COLOR);
+        //自定义精度范围的圆形边框宽度
+        myLocationStyle.strokeWidth(5);
+        // 设置圆形的填充颜色
+        myLocationStyle.radiusFillColor(FILL_COLOR);
+        // 将自定义的 myLocationStyle 对象添加到地图上
+        aMap.setMyLocationStyle(myLocationStyle);
+    }
+}
---
+++ app/src/main/java/com/yishu/asumap/Util.java
@@ -0,0 +1,29 @@
+package com.yishu.asumap;
+
+import android.content.Context;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
+
+/**
+ * Created by simon on 17/5/23.
+ */
+
+public class Util {
+    public static boolean isNetworkAvailable(Context context) {
+        ConnectivityManager cm = (ConnectivityManager) context
+                .getSystemService(Context.CONNECTIVITY_SERVICE);
+        if (cm != null) {
+            //如果仅仅是用来判断网络连接
+            //则可以使用 cm.getActiveNetworkInfo().isAvailable();
+            NetworkInfo[] info = cm.getAllNetworkInfo();
+            if (info != null) {
+                for (int i = 0; i < info.length; i++) {
+                    if (info[i].getState() == NetworkInfo.State.CONNECTED) {
+                        return true;
+                    }
+                }
+            }
+        }
+        return false;
+    }
+}
